580c33f769607553fc10b3adcd3caaeb954e68a8
Sofia Simanke <sofia@simanke.design>
2020-05-21 13:15:00 -0300

    Added logo

---
+++ components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <StoreLogo />
+    <nav class="bg-indigo-500">Test</nav>
+  </div>
+</template>
+
+<script>
+import StoreLogo from '@/components/StoreLogo.vue'
+export default {
+  components: {
+    StoreLogo
+  }
+}
+</script>
+
+<style lang="pcss"></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,30 @@
  */
 module.exports = {
   theme: {
-    fontFamily: {
-      body: ['"Source Sans Pro"', 'sans-serif']
-    },
-    colors: {
-      green: '#587F14',
-      'green-light': '#A8BC85',
-      'green-lighter': '#E9F0DC',
-      brown: '#B19D86',
-      'brown-light': '#CBC3AF',
-      'brown-lighter': '#EDE9E4'
-    },
-    variants: {},
-    plugins: []
+    extend: {
+      fontFamily: {
+        body: ['"Source Sans Pro"', 'sans-serif']
+      },
+      colors: {
+        green: '#587F14',
+        'green-light': '#A8BC85',
+        'green-lighter': '#E9F0DC',
+        brown: '#B19D86',
+        'brown-light': '#CBC3AF',
+        'brown-lighter': '#EDE9E4'
+      }
+    }
+  },
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    // process.env.NODE_ENV === 'production'
+    enabled: false,
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
   }
 }
